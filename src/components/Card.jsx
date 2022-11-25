c2d2a834f0532627011309256577773671f3d8fa
Vitalii <rebrun508@gmail.com>
2022-11-25 13:20:43 +0200

    UI complete 90%

---
+++ src/components/Card.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: ${(props) => props.type !== "sm" && "360px"};
+  margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
+  cursor: pointer;
+  display: ${(props) => props.type === "sm" && "flex"};
+  gap: 10px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
+  background-color: #999;
+  flex: 1;
+`;
+
+const Details = styled.div`
+  display: flex;
+  margin-top: ${(props) => props.type !== "sm" && "16px"};
+  gap: 12px;
+  flex: 1;
+`;
+
+const ChannelImage = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+  display: ${(props) => props.type === "sm" && "none"};
+`;
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Texts = styled.div``;
+
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`;
+
+const Info = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Card = ({ type }) => {
+  return (
+    <Link to="/video/test" style={{ textDecoration: "none" }}>
+      <Container type={type}>
+        <Image
+          type={type}
+          src="https://i9.ytimg.com/vi_webp/k3Vfj-e1Ma4/mqdefault.webp?v=6277c159&sqp=CIjm8JUG&rs=AOn4CLDeKmf_vlMC1q9RBEZu-XQApzm6sA"
+        />
+        <Details type={type}>
+          <Avatar src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"/>
+          <Texts>
+              <Title>Тестовое Видео</Title>
+              <ChannelName>Rebrun16</ChannelName>
+              <Info>658,367  просмотров • 1 день назад</Info>
+            </Texts>
+        </Details>
+      </Container>
+    </Link>
+  );
+};
+
+export default Card;
